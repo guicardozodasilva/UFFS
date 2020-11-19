b2d1451b9c353d6cba69c5c4c335dd44a7f1eae0
Guilherme Cardoz da Silva <guilherme.silva@estudante.uffs.edu.br>
2020-11-19 16:09:48 -0300

    - Adicionado o ground (chão).

--- Computação Gráfica/Atividade04/js/primeiro.js
+++ Computação Gráfica/Atividade04/js/primeiro.js
@@ -7,30 +7,37 @@ var create = function() {
     const geometryCircleBuffer = new THREE.CircleBufferGeometry(7, 24, Math.PI * 0.25, Math.PI * 1.5);
     const geometryIcosahedron = new THREE.IcosahedronBufferGeometry(7);
     const geometryOctahedron = new THREE.OctahedronBufferGeometry(7);
+    const geometryGround = new THREE.PlaneBufferGeometry(100,100, 10);
 
     const materialCircleBuffer = new THREE.MeshBasicMaterial( { color: 'red' } );
     const materialIcosahedron = new THREE.MeshBasicMaterial( { color: 'blue' } );
     const materialOctahedron = new THREE.MeshBasicMaterial( { color: 'magenta' } );
+    const materialGround = new THREE.MeshBasicMaterial( { color: 'green' } );
 
     circleBuffer = new THREE.Mesh(geometryCircleBuffer, materialCircleBuffer);
     icosahedron = new THREE.Mesh(geometryIcosahedron, materialIcosahedron);
     octahedron = new THREE.Mesh(geometryOctahedron, materialOctahedron);
+    ground = new THREE.Mesh(geometryGround, materialGround);
 
     scene.add(circleBuffer);
     scene.add(icosahedron);
     scene.add(octahedron);
+    scene.add(ground);
     
     circleBuffer.position.x = 15;
     circleBuffer.position.y = 15;
     circleBuffer.position.z = 10;
 
     icosahedron.position.x = -1;
-    icosahedron.position.y = -14;
+    icosahedron.position.y = -5;
     icosahedron.position.z = 0;
 
     octahedron.position.x = -25;
     octahedron.position.y = 20;
     octahedron.position.z = 9;
+
+    ground.rotation.x = - Math.PI / 2; // rotaciona para que ela fique paralela ao eixo X
+    ground.position.y -= 30; // Posiciona o ground abaixo da nossa figura.
 
 };
 
