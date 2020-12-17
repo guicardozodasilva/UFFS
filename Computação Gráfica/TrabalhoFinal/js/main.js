a047ebc06ed4b330b871be248777b8ed8828248b
Guilherme Cardoz da Silva <guilherme.silva@estudante.uffs.edu.br>
2020-12-17 02:20:45 -0300

    -> correcao da iluminacao -> adicionado o personagem -> adicionado alguns arquivos js no projeto -> adicionado arquivos de animacoes

--- Computação Gráfica/TrabalhoFinal/js/main.js
+++ Computação Gráfica/TrabalhoFinal/js/main.js
@@ -2,7 +2,19 @@ var scene;
 var camera;
 var renderer;
 var controls;
+
 var skybox;
+var plane;
+
+var objLoader;
+var textureLoader;
+
+//iluminação
+var spotLight;
+var ambientLight;
+var directionalLight;
+
+var objCarregado = [];
 
 var criaGround = function (){
 
@@ -25,7 +37,17 @@ var criaGround = function (){
        materialArray[i].side = THREE.BackSide;
     var skyboxGeo = new THREE.BoxGeometry( 10000, 10000, 10000);
     skybox = new THREE.Mesh( skyboxGeo, materialArray );
-    scene.add( skybox );  
+    scene.add( skybox );
+    
+    plane = new THREE.Mesh(
+        new THREE.PlaneGeometry(100, 100, 10, 10),
+        new THREE.MeshStandardMaterial({
+            color: 0x808080,
+          }));
+    plane.castShadow = false;
+    plane.receiveShadow = true;
+    plane.rotation.x = -Math.PI / 2;
+    scene.add(plane);
 
 }
 
@@ -43,6 +65,7 @@ var iluminacaoDirectional = function(){
     directionalLight.shadow.camera.top = -1000;
 
     directionalLight.target = skybox;
+    directionalLight.target = plane;
 
     scene.add(directionalLight);
     scene.add(directionalLight.target);
@@ -51,11 +74,47 @@ var iluminacaoDirectional = function(){
 
 }
 
+var loadObj = function(){
+    objLoader = new THREE.OBJLoader();
+    fbxLoader = new THREE.FBXLoader();
+    textureLoader = new THREE.TextureLoader();
+    
+    fbxLoader.load( 'assets/models/adam.fbx', function ( object ) {
+        objCarregado.push(object);
+        object.scale.set(0.25, 0.25, 0.25);
+
+        object.traverse( function ( child ) {
+
+            if ( child.isMesh ) {
+
+                child.material.shininess = 0;
+                child.castShadow = true;
+                child.receiveShadow = true;
+
+            }
+
+        } );    
+
+        object.position.z = 0;
+        object.position.x = 0;
+        object.position.y = 0;
+        object.rotation.y = 5;
+        scene.add( object );
+
+    } );
+
+}
+
 var init = function() {
 
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(55,window.innerWidth/window.innerHeight,45,30000);
-    camera.position.set(-900,-200,-900);
+
+    const fov = 60;
+    const aspect = 1920 / 1080;
+    const near = 1;
+    const far = 10000;
+    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(10, 20, -100);
 
     renderer = new THREE.WebGLRenderer({antialias:true});
     renderer.setSize(window.innerWidth,window.innerHeight);
@@ -64,12 +123,15 @@ var init = function() {
 
     controls = new THREE.OrbitControls(camera, renderer.domElement);
     controls.addEventListener('change', renderer);
-    controls.minDistance = 500;
-    controls.maxDistance = 1500;
 
     criaGround();
 
+    loadObj();
+
     iluminacaoDirectional();
+    ambientLight = new THREE.AmbientLight( 0x888888 );
+    ambientLight.intensity = 1;
+    scene.add(ambientLight);
 
     render();
 
